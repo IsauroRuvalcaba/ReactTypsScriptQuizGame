b2b85d966160d7db0f371d76365279b8344e2675
Isauro Ruvalcaba <isauroruvalcaba@sbcglobal.net>
2024-05-16 22:32:50 -0700

    created a function that fetches question data from an api

--- src/App.tsx
+++ src/App.tsx
@@ -2,24 +2,32 @@ import "./App.scss";
 import Score from "./components/Score.tsx";
 import Game from "./components/Game.tsx";
 import { useQuiz } from "./QuizContext.tsx";
+import { useEffect } from "react";
 
 function App() {
-  const { state, dispatch } = useQuiz();
+  const { state } = useQuiz();
 
-  console.log(state);
+  // console.log(state);
+
+  async function fetchQuestion() {
+    const response = await fetch(
+      "https://opentdb.com/api.php?amount=1&category=18"
+    );
+    let data = await response.json();
+    let question = data.results[0];
+    console.log(data);
+  }
+
+  useEffect(() => {
+    if (state.gameStatus == "idle") {
+      fetchQuestion();
+    }
+  });
 
   return (
     <>
       <Score />
       <Game />
-      <h2>Status: {state.gameStatus}</h2>
-      <button
-        onClick={() => {
-          dispatch({ type: "setStatus", payload: "fetching" });
-        }}
-      >
-        Set Loading
-      </button>
     </>
   );
 }
