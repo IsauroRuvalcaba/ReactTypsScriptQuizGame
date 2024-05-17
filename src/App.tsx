abe9e633a4b2aaa5cd44edf0d6698f893e6aeee6
Isauro Ruvalcaba <isauroruvalcaba@sbcglobal.net>
2024-05-16 19:09:00 -0700

    created the QuizProvider function and exported that to main.tsx. Sorrounded the the App comonent with with imported QuizProvider in main.tsx. Created a useContext function in the QuizContext.tsx file to export to any file to be able to use the data provided by QuizContext. Imported this function of useContext to App.tsx to use it there.

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
 import "./App.scss";
 import Score from "./components/Score.tsx";
 import Game from "./components/Game.tsx";
+import { useQuiz } from "./QuizContext.tsx";
 
 function App() {
+  const state = useQuiz();
+
+  console.log(state);
+
   return (
     <>
       <Score />
--- src/QuizContext.tsx
+++ src/QuizContext.tsx
@@ -10,16 +10,36 @@ const initialState: QuizState = {
 
 const QuizContext = createContext<QuizState>(initialState);
 
+export function QuizProvider({ children }: { children: React.ReactElement }) {
+  const [state, setState] = useState(initialState);
+
+  // the {children} is where <App/> goes when imported in the main.tsx
+  return <QuizContext.Provider value={state}>{children}</QuizContext.Provider>;
+}
+
+// this will allow any file that imports this to access the state.  This is used instead of that file importing useContext and also importing QuizContext from this file
+export function useQuiz() {
+  return useContext(QuizContext);
+}
+
 /* 
 
 *
 *
 * Actual js code
 
-import { createContext } from "react";
+import { createContext, useContext, useState } from "react";
 const initialState = {
     gameStatus: "idle",
 };
 const QuizContext = createContext(initialState);
+export function QuizProvider({ children }) {
+    const [state, setState] = useState(initialState);
+    return React.createElement(QuizContext.Provider, { value: state }, children);
+}
+
+export function useQuiz() {
+    return useContext(QuizContext);
+}
 
 */
